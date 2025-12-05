85d1783e8a2fdfb0b1464e4482c465cd112eb5ea
webdeveloper510 <developerweb6@gmail.com>
2025-12-05 17:53:31 +0530

    updated build

--- assets/CModalFooter-B8YreNiz.js
+++
@@ -1,1 +0,0 @@
-import{r as t,_ as d,R as m,a as n,c as i,P as e}from"./index-X8N2_xbW.js";var s=t.forwardRef(function(a,r){var o=a.children,c=a.className,l=d(a,["children","className"]);return m.createElement("div",n({className:i("modal-footer",c)},l,{ref:r}),o)});s.propTypes={children:e.node,className:e.string};s.displayName="CModalFooter";export{s as C};
